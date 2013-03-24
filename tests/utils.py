de3fc5f9928a689918bd019fdd225d92987f4b80
Bogdan Neacsa <neacsa_bogdan_valentin@yahoo.com>
2013-03-24 19:29:33 +0200

    Changes to utils.py to add proper stemming and stopwords removal.

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,9 @@
 import os
 from gensim import corpora, models, similarities
+import nltk.corpus as sw_corpus
+import nltk.stem.porter as porter
+
+STEMMER = porter.PorterStemmer()
 
 from xml.dom.minidom import parseString
 
@@ -16,9 +20,9 @@ class SemevalCorpus():
         self.sentence_data = self.parse_semeval_data(data_file, valence_file)
         self.storage_file = 'corpus_%s.mm'%(id(self),)
         # TODO: use actual stemmers and stowords removal from either NLTK or stemming module
-        dictionary = corpora.Dictionary(entry.sentence.lower().split() for entry in self.sentence_data.values())
-        stoplist = set('for a of the and to in'.split())
-        stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
+        data = [[self.preprocess_word(word) for word in entry.sentence.lower().split()] for entry in self.sentence_data.values()]
+        dictionary = corpora.Dictionary(data)
+        stop_ids = [dictionary.token2id[stopword] for stopword in sw_corpus.stopwords.words('english')
                                                             if stopword in dictionary.token2id]
         once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
         dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
@@ -28,6 +32,9 @@ class SemevalCorpus():
     def __del__(self):
         if os.path.exists(self.storage_file):
             self.clear()
+        
+    def preprocess_word(self, word):
+        return STEMMER.stem(word.replace(',', '').replace('"', ''))
         
     def parse_semeval_data(self, xml_file, valence_file):
         results_dict = {}
@@ -66,7 +73,7 @@ class SemevalCorpus():
         for line in self.sentence_data.values():
             yield self.dictionary.doc2bow(line.sentence.lower().split())
             
-    def to_sparse_arff(self, output_file, corpus_type='tfidf'):
+    def to_sparse_arff(self, output_file, corpus_type='tfidf', valence_class=None):
         if corpus_type == 'tfidf':
             tfidf = models.TfidfModel(self.mm_corpus())
             words = []
@@ -79,16 +86,27 @@ class SemevalCorpus():
                 fp.write('@relation tfidf_features\n\n')
                 for word in words:
                     fp.write("@attribute %s numeric\n"%(word[0]))
-                fp.write("@attribute valence numeric\n\n")
+                if valence_class is None:
+                    fp.write("@attribute valence numeric\n\n")
+                else:
+                    fp.write("@attribute valence {-1,0,1}\n\n")
                 fp.write("@data\n")
                 for sent_id in self.sentence_data:
                     sparse_vector_rep = tfidf[self.vector_for_id(sent_id)]
-                    data_string = '{'
-                    for entry in sparse_vector_rep:
-                        data_string += '%s %s,'%(entry[0], entry[1])
-                    data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].valence)
-                    fp.write(data_string)
-                
+                    if sparse_vector_rep:
+                        data_string = '{'
+                        for entry in sparse_vector_rep:
+                            data_string += '%s %s,'%(entry[0], entry[1])
+                        if valence_class is None:
+                            data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].valence)
+                        else:
+                            valence = 0
+                            if int(self.sentence_data[sent_id].valence) > 0:
+                                valence = 1
+                            if int(self.sentence_data[sent_id].valence) < 0:
+                                valence = -1
+                            data_string += '%s %s}\n'%(len(words), valence)
+                        fp.write(data_string)
                     
             
     def mm_corpus(self):
@@ -96,47 +114,7 @@ class SemevalCorpus():
             self.serialize()
         return corpora.MmCorpus(self.storage_file)
 
-#documents = ["Human machine interface for lab abc computer applications",
-#             "A survey of user opinion of computer system response time",
-#             "The EPS user interface management system",
-#             "System and human system engineering testing of EPS",
-#             "Relation of user perceived response time to error measurement",
-#             "The generation of random binary unordered trees",
-#             "The intersection graph of paths in trees",
-#             "Graph minors IV Widths of trees and well quasi ordering",
-#             "Graph minors A survey"]
-#
-#stoplist = set('for a of the and to in'.split())
-#texts = [[word for word in document.lower().split() if word not in stoplist]
-#            for document in documents]
-#
-## remove words that appear only once
-#all_tokens = sum(texts, [])
-#tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-#texts = [[word for word in text if word not in tokens_once]
-#          for text in texts]
-#
-#dictionary = corpora.Dictionary(texts)
-#new_doc = "Human computer interaction"
-#new_vec = dictionary.doc2bow(new_doc.lower().split())
 
 corpus_memory_friendly = SemevalCorpus('affectivetext_trial.xml', 'affectivetext_trial.valence.gold')
-corpus_memory_friendly.to_sparse_arff('sample.arff')
+corpus_memory_friendly.to_sparse_arff('sample-binary.arff', valence_class='b')
 
-#mm_corpus = corpus_memory_friendly.mm_corpus()
-#tfidf = models.TfidfModel(mm_corpus)
-#
-##print corpus_memory_friendly.sentence_for_id('1')
-#print corpus_memory_friendly.vector_for_id('1')
-#print tfidf[corpus_memory_friendly.vector_for_id('1')]
-#    
-#corpora.MmCorpus.serialize('corpus.mm', corpus_memory_friendly)
-#corpus = corpora.MmCorpus('corpus.mm')
-#print corpus
-#
-#tfidf = models.TfidfModel(corpus)
-##input_txt = corpus.doc2bow("Once upon a time there was a village shop.".lower().split())
-#for doc in tfidf[corpus]:
-#    print doc
-
-#print new_vec
