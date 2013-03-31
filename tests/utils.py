d8d11eb57717d8dbbad22a2c8d3ae93dbcac3457
Bogdan Neacsa <neacsa_bogdan_valentin@yahoo.com>
2013-03-31 15:58:45 +0300

    Added lsi, lda models and better stemming procedure.

--- tests/utils.py
+++ tests/utils.py
@@ -18,32 +18,46 @@ class Sentence():
 
 class FairytaleCorpus():
     
-    def __init__(self, data_folder):
+    def __init__(self, data_folders):
         dict_data = {}
         start_idx = 0
-        for data_file in os.listdir(data_folder):
-            results_dict, end_idx = self.parse_fairytale_data(os.path.join(data_folder, data_file), start_idx)
-            dict_data.update(results_dict)
-            start_idx += end_idx
+        for data_folder in data_folders:
+            for data_file in os.listdir(data_folder):
+                results_dict, end_idx = self.parse_fairytale_data(os.path.join(data_folder, data_file), start_idx)
+                dict_data.update(results_dict)
+                start_idx += end_idx
         self.sentence_data = dict_data
         self.storage_file = 'corpus_%s.mm'%(id(self),)
         data = [[self.preprocess_word(word) for word in entry.sentence.lower().split()] for entry in self.sentence_data.values()]
         dictionary = corpora.Dictionary(data)
-        stop_ids = [dictionary.token2id[stopword] for stopword in sw_corpus.stopwords.words('english')
+        stopwords = sw_corpus.stopwords.words('english')
+        #Just remove empty string here
+        stopwords.append('')
+        stop_ids = [dictionary.token2id[stopword] for stopword in stopwords
                                                             if stopword in dictionary.token2id]
         once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
         dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
         dictionary.compactify()
         self.dictionary = dictionary
     
+    
     def parse_fairytale_data(self, file_name, start_idx=0):
+        """
+        For this file name, parse all sentences and return a dictionary
+        of the form {sentence_id : Sentence()}
+        """
         results_dict = {}
         for line in open(file_name):
             sent_id, sentence = self.process_line(line)
             results_dict[str(int(sent_id)+start_idx)] = sentence
         return results_dict, int(sent_id) + start_idx
     
+    
     def process_line(self, line):
+        """
+        Process a given line from the fairytale corpus, return a tuple in the form
+        (sent_id : Sentence() entity)
+        """
         first_index = line.index(':')
         sent_id = line[:first_index]
         second_index = first_index + line[first_index+1:].index(':') + 1
@@ -58,12 +72,22 @@ class FairytaleCorpus():
             self.clear()
         
     def preprocess_word(self, word):
-        return STEMMER.stem(word.replace(',', '').replace('"', '').replace("'", ''))
+        """
+        Do preprocessing on a word, remove all non letter characters and stem the word.
+        """
+        new_word = ''.join([ch for ch in word if ch.isalnum() and not ch.isdigit()])
+        return STEMMER.stem(new_word)
     
     def sentence_for_id(self, sent_id):
+        """
+        Return the sentence for this id.
+        """
         return self.sentence_data.get(sent_id, None).sentence
     
     def vector_for_id(self, sent_id):
+        """
+        For the given sentence return the vector representation using our dictionary.
+        """
         sent = self.sentence_data.get(sent_id, None)
         if sent is None:
             # TODO: Add entry here with unknown valence ??
@@ -82,130 +106,49 @@ class FairytaleCorpus():
     def __iter__(self):
         for line in self.sentence_data.values():
             yield self.dictionary.doc2bow(line.sentence.lower().split())
+            
+    def tfidf_model(self):
+        return models.TfidfModel(self.mm_corpus())
+            
+    def lsi_model(self):
+        return models.LsiModel(self.tfidf_model()[self.mm_corpus()], id2word=self.dictionary, num_topics=200)
+            
+    def lda_model(self):
+        return models.LdaModel(self.mm_corpus(), id2word=self.dictionary, num_topics=150)
             
     def to_sparse_arff(self, output_file, corpus_type='tfidf'):
+        """
+        Write out the corpus in sparse arff format, with the corpus type being either:
+         - tfidf, lsi, lda
+        """
         if corpus_type == 'tfidf':
-            tfidf = models.TfidfModel(self.mm_corpus())
-            words = []
-            for word in self.dictionary.token2id:
-                words.append((word, self.dictionary.token2id[word]))
-            # Sort the words after the index so we can write our arff file.
-            words = sorted(words, key=lambda x: x[1])
-            print "Corpus has %i words"%(len(words))
-            with open(output_file, 'w') as fp:
-                fp.write('@relation tfidf_features\n\n')
-                for word in words:
-                    fp.write("@attribute %s numeric\n"%(word[0]))
-                fp.write("@attribute primary_emotion {A,D,F,H,N,Sa,Su+,Su-}\n\n")
-                fp.write("@data\n")
-                for sent_id in self.sentence_data:
-                    sparse_vector_rep = tfidf[self.vector_for_id(sent_id)]
-                    if sparse_vector_rep:
-                        data_string = '{'
-                        for entry in sparse_vector_rep:
-                            data_string += '%s %s,'%(entry[0], entry[1])
-                        data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].primary_emotion)
-                        fp.write(data_string)
-                    
-            
-    def mm_corpus(self):
-        if not os.path.exists(self.storage_file):
-            self.serialize()
-        return corpora.MmCorpus(self.storage_file)
-            
-
-class SemevalCorpus():
-    
-    def __init__(self, data_file, valence_file):
-        self.sentence_data = self.parse_semeval_data(data_file, valence_file)
-        self.storage_file = 'corpus_%s.mm'%(id(self),)
-        data = [[self.preprocess_word(word) for word in entry.sentence.lower().split()] for entry in self.sentence_data.values()]
-        dictionary = corpora.Dictionary(data)
-        stop_ids = [dictionary.token2id[stopword] for stopword in sw_corpus.stopwords.words('english')
-                                                            if stopword in dictionary.token2id]
-        once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-        dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
-        dictionary.compactify()
-        self.dictionary = dictionary
-        
-    def __del__(self):
-        if os.path.exists(self.storage_file):
-            self.clear()
-        
-    def preprocess_word(self, word):
-        return STEMMER.stem(word.replace(',', '').replace('"', '').replace("'", ''))
-        
-    def parse_semeval_data(self, xml_file, valence_file):
-        results_dict = {}
-        data_string = open(xml_file, 'r').read()
-        dom = parseString(data_string)
-        instances = dom.getElementsByTagName('instance')
-        for instance in instances:
-            results_dict[instance.attributes['id'].nodeValue] = {'value' : instance.firstChild.nodeValue}
-        for line in open(valence_file):
-            sent_id, valence = line.split()
-            results_dict[sent_id]['valence'] = valence
-        for key in results_dict:
-            results_dict[key] = Sentence(results_dict[key]['value'], results_dict[key]['valence'])
-        return results_dict
-        
-    def sentence_for_id(self, sent_id):
-        return self.sentence_data.get(sent_id, None).sentence
-    
-    def vector_for_id(self, sent_id):
-        sent = self.sentence_data.get(sent_id, None)
-        if sent is None:
-            # TODO: Add entry here with unknown valence ??
-            raise Exception("No sentence stored so far!")
-        return self.dictionary.doc2bow(sent.sentence.lower().split())
-    
-    def clear(self):
-        if os.path.exists(self.storage_file):
-            os.remove(self.storage_file)
-        if os.path.exists(self.storage_file + '.index'):
-            os.remove(self.storage_file + '.index')
-    
-    def serialize(self):
-        corpora.MmCorpus.serialize(self.storage_file, self)
-        
-    def __iter__(self):
-        for line in self.sentence_data.values():
-            yield self.dictionary.doc2bow(line.sentence.lower().split())
-            
-    def to_sparse_arff(self, output_file, corpus_type='tfidf', valence_class=None):
-        if corpus_type == 'tfidf':
-            tfidf = models.TfidfModel(self.mm_corpus())
-            words = []
-            for word in self.dictionary.token2id:
-                words.append((word, self.dictionary.token2id[word]))
-            # Sort the words after the index so we can write our arff file.
-            words = sorted(words, key=lambda x: x[1])
-            print "Corpus has %i words"%(len(words))
-            with open(output_file, 'w') as fp:
-                fp.write('@relation tfidf_features\n\n')
-                for word in words:
-                    fp.write("@attribute %s numeric\n"%(word[0]))
-                if valence_class is None:
-                    fp.write("@attribute valence numeric\n\n")
-                else:
-                    fp.write("@attribute valence {-1,0,1}\n\n")
-                fp.write("@data\n")
-                for sent_id in self.sentence_data:
-                    sparse_vector_rep = tfidf[self.vector_for_id(sent_id)]
-                    if sparse_vector_rep:
-                        data_string = '{'
-                        for entry in sparse_vector_rep:
-                            data_string += '%s %s,'%(entry[0], entry[1])
-                        if valence_class is None:
-                            data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].valence)
-                        else:
-                            valence = 0
-                            if int(self.sentence_data[sent_id].valence) > 0:
-                                valence = 1
-                            if int(self.sentence_data[sent_id].valence) < 0:
-                                valence = -1
-                            data_string += '%s %s}\n'%(len(words), valence)
-                        fp.write(data_string)
+            model = self.tfidf_model()
+        elif corpus_type == 'lsi':
+            model = self.lsi_model()
+        elif corpus_type == 'lda':
+            model = self.lda_model()
+        else:
+            raise Exception("Unknown type %s"%(corpus_type,))
+        words = []
+        for word in self.dictionary.token2id:
+            words.append((word, self.dictionary.token2id[word]))
+        # Sort the words after the index so we can write our arff file.
+        words = sorted(words, key=lambda x: x[1])
+        print "Corpus has %i words"%(len(words))
+        with open(output_file, 'w') as fp:
+            fp.write('@relation tfidf_features\n\n')
+            for word in words:
+                fp.write("@attribute %s numeric\n"%(word[0]))
+            fp.write("@attribute primary_emotion {A,D,F,H,N,Sa,Su+,Su-}\n\n")
+            fp.write("@data\n")
+            for sent_id in self.sentence_data:
+                sparse_vector_rep = model[self.vector_for_id(sent_id)]
+                if sparse_vector_rep:
+                    data_string = '{'
+                    for entry in sparse_vector_rep:
+                        data_string += '%s %s,'%(entry[0], entry[1])
+                    data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].primary_emotion)
+                    fp.write(data_string)
                     
             
     def mm_corpus(self):
@@ -213,11 +156,10 @@ class SemevalCorpus():
             self.serialize()
         return corpora.MmCorpus(self.storage_file)
 
-
-#corpus_memory_friendly = SemevalCorpus('affectivetext_trial.xml', 'affectivetext_trial.valence.gold')
-#corpus_memory_friendly.to_sparse_arff('sample-binary.arff', valence_class='b')
-grimm_data_folder = os.sep.join(['..', 'fairytales', 'Grimms', 'emmood'])
-fairy_corpus = FairytaleCorpus(grimm_data_folder)
-fairy_corpus.to_sparse_arff('fairytale.arff')
+grimm_data_folders = [os.sep.join(['..', 'fairytales', 'Grimms', 'emmood']), os.sep.join(['..', 'fairytales', 'HCAndersen', 'emmood']), os.sep.join(['..', 'fairytales', 'Potter', 'emmood']), ]
+fairy_corpus = FairytaleCorpus(grimm_data_folders)
+#fairy_corpus.to_sparse_arff('fairytale_tfidf.arff', 'tfidf')
+#fairy_corpus.to_sparse_arff('fairytale_lsi.arff', 'lsi')
+#fairy_corpus.to_sparse_arff('fairytale_lda.arff', 'lda')
 
 
---
+++ tests/utils_semeval.py
@@ -0,0 +1,110 @@
+import os
+from gensim import corpora, models, similarities
+import nltk.corpus as sw_corpus
+import nltk.stem.porter as porter            
+
+class SemevalCorpus():
+    
+    def __init__(self, data_file, valence_file):
+        self.sentence_data = self.parse_semeval_data(data_file, valence_file)
+        self.storage_file = 'corpus_%s.mm'%(id(self),)
+        data = [[self.preprocess_word(word) for word in entry.sentence.lower().split()] for entry in self.sentence_data.values()]
+        dictionary = corpora.Dictionary(data)
+        stopwords = sw_corpus.stopwords.words('english')
+        #Just remove empty string here
+        stopwords.append('')
+        stop_ids = [dictionary.token2id[stopword] for stopword in stopwords
+                                                            if stopword in dictionary.token2id]
+        once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+        dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
+        dictionary.compactify()
+        self.dictionary = dictionary
+        
+    def __del__(self):
+        if os.path.exists(self.storage_file):
+            self.clear()
+        
+    def preprocess_word(self, word):
+        return STEMMER.stem(word.replace(',', '').replace('"', '').replace("'", ''))
+        
+    def parse_semeval_data(self, xml_file, valence_file):
+        results_dict = {}
+        data_string = open(xml_file, 'r').read()
+        dom = parseString(data_string)
+        instances = dom.getElementsByTagName('instance')
+        for instance in instances:
+            results_dict[instance.attributes['id'].nodeValue] = {'value' : instance.firstChild.nodeValue}
+        for line in open(valence_file):
+            sent_id, valence = line.split()
+            results_dict[sent_id]['valence'] = valence
+        for key in results_dict:
+            results_dict[key] = Sentence(results_dict[key]['value'], results_dict[key]['valence'])
+        return results_dict
+        
+    def sentence_for_id(self, sent_id):
+        return self.sentence_data.get(sent_id, None).sentence
+    
+    def vector_for_id(self, sent_id):
+        sent = self.sentence_data.get(sent_id, None)
+        if sent is None:
+            # TODO: Add entry here with unknown valence ??
+            raise Exception("No sentence stored so far!")
+        return self.dictionary.doc2bow(sent.sentence.lower().split())
+    
+    def clear(self):
+        if os.path.exists(self.storage_file):
+            os.remove(self.storage_file)
+        if os.path.exists(self.storage_file + '.index'):
+            os.remove(self.storage_file + '.index')
+    
+    def serialize(self):
+        corpora.MmCorpus.serialize(self.storage_file, self)
+        
+    def __iter__(self):
+        for line in self.sentence_data.values():
+            yield self.dictionary.doc2bow(line.sentence.lower().split())
+            
+    def to_sparse_arff(self, output_file, corpus_type='tfidf', valence_class=None):
+        if corpus_type == 'tfidf':
+            tfidf = models.TfidfModel(self.mm_corpus())
+            words = []
+            for word in self.dictionary.token2id:
+                words.append((word, self.dictionary.token2id[word]))
+            # Sort the words after the index so we can write our arff file.
+            words = sorted(words, key=lambda x: x[1])
+            print "Corpus has %i words"%(len(words))
+            with open(output_file, 'w') as fp:
+                fp.write('@relation tfidf_features\n\n')
+                for word in words:
+                    fp.write("@attribute %s numeric\n"%(word[0]))
+                if valence_class is None:
+                    fp.write("@attribute valence numeric\n\n")
+                else:
+                    fp.write("@attribute valence {-1,0,1}\n\n")
+                fp.write("@data\n")
+                for sent_id in self.sentence_data:
+                    sparse_vector_rep = tfidf[self.vector_for_id(sent_id)]
+                    if sparse_vector_rep:
+                        data_string = '{'
+                        for entry in sparse_vector_rep:
+                            data_string += '%s %s,'%(entry[0], entry[1])
+                        if valence_class is None:
+                            data_string += '%s %s}\n'%(len(words), self.sentence_data[sent_id].valence)
+                        else:
+                            valence = 0
+                            if int(self.sentence_data[sent_id].valence) > 0:
+                                valence = 1
+                            if int(self.sentence_data[sent_id].valence) < 0:
+                                valence = -1
+                            data_string += '%s %s}\n'%(len(words), valence)
+                        fp.write(data_string)
+                    
+            
+    def mm_corpus(self):
+        if not os.path.exists(self.storage_file):
+            self.serialize()
+        return corpora.MmCorpus(self.storage_file)
+
+
+#corpus_memory_friendly = SemevalCorpus('affectivetext_trial.xml', 'affectivetext_trial.valence.gold')
+#corpus_memory_friendly.to_sparse_arff('sample-binary.arff', valence_class='b')
